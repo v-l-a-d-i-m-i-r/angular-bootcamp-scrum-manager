d633882064c185ea81bac544145b7a3d3907c0b4
Nickolay Kofanov <nkofanov@schoology.com>
2016-05-18 16:23:50 +0300

    Add client side

---
+++ client/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('scrumManager', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
